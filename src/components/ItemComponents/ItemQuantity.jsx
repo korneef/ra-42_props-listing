3e7537c04cd586aef9d7a1ca0bbafafd159a42e9
Valentin Korneev <v.korneef@gmail.com>
2022-12-06 18:18:55 +0600

    delete comment on file

--- src/components/ItemComponents/ItemQuantity.jsx
+++ src/components/ItemComponents/ItemQuantity.jsx
@@ -4,7 +4,3 @@ export default function ItemQuantity({ quantity }) {
   (quantity > 20) && (level = 'level-high');
   return <p className={`item-quantity ${level}`}>{quantity} left</p>
 }
-
-// level-low — если остаток меньше 10 включительно;
-// level-medium — если остаток меньше 20 включительно;
-// level-high — если остаток больше 20.
